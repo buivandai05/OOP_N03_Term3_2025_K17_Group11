acf1394b7b0aa16e7628fdbdab67bb40084d3127
camlee <150875672+camlee05@users.noreply.github.com>
2025-05-09 09:21:58 +0700

    Update TestKhoa.java

--- test/TestKhoa.java
+++ test/TestKhoa.java
@@ -1,1 +1,7 @@
+public class TestKhoa {
+    public static void main(String[] args) {
+        Khoa khoa1 = new Khoa("W001", "Khoa Nội", 40);
+        Khoa.displayInfo(); // In thông tin khoa
+    }
+}
 
