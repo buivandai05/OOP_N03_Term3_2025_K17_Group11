6adb91b9b480ebbd450beb008a0266e754e22606
camlee <150875672+camlee05@users.noreply.github.com>
2025-05-09 09:02:57 +0700

    Update VienPhi.java

--- src/VienPhi.java
+++ src/VienPhi.java
@@ -1,1 +1,63 @@
+public class VienPhi {
+    private String maVienPhi;     // Mã viện phí
+    private BenhNhan benhNhan;     // Bệnh nhân liên quan (đối tượng Patient)
+    private double phiKham;      // Chi phí khám bệnh
+    private double phiThuoc;     // Chi phí thuốc
+    private double phiPhong;    // Chi phí giường bệnh (nếu có)
+    private double tongVienPhi;
+
+    public VienPhi(String maVienPhi, Patient benhNhan, double phiKham,
+                   double phiPhong, double phiThuoc, double tongVienPhi) {
+        this.maVienPhi = maVienPhi;
+        this.benhNhan = benhNhan;
+        this.phiKham = phiKham;
+        this.phiThuoc = phiThuoc;
+        this.tongVienPhi = tongVienPhi;
+    }
+
+    public String getMaVienPhi() {
+        return maVienPhi;
+    }
+
+    public void setMaVienPhi(String maVienPhi) {
+        this.maVienPhi = maVienPhi;
+    }
+
+    public BenhNhan getBenhNhan() {
+        return benhNhan;
+    }
+
+    public void setBenhNhan(BenhNhan benhNhan) {
+        this.benhNhan = benhNhan;
+    }
+
+    public double getPhiKham() {
+        return phiKham;
+    }
+
+    public void setPhiKham(double phiKham) {
+        this.phiKham = phiKham;
+    }
+
+    public double getPhiThuoc() {
+        return phiThuoc;
+    }
+
+    public void setPhiThuoc(double phiThuoc) {
+        this.phiThuoc = phiThuoc;
+    }
+
+    public double getPhiPhong() {
+        return phiPhong;
+    }
+
+    public void setPhiPhong(double phiPhong) {
+        this.phiPhong = phiPhong;
+    }
+
+    public double tinhTongChiPhi() {
+        return phiKham + phiThuoc + phiPhong;
+    }
+
+}
 
