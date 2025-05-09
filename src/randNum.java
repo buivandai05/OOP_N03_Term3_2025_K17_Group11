bef72d14acd669d63872e7f4d567baee426ca140
buivandai05 <151592182+buivandai05@users.noreply.github.com>
2025-05-09 07:36:33 +0700

    Update randNum.java

--- src/randNum.java
+++ src/randNum.java
@@ -1,1 +1,10 @@
+public class randNum {
+ public static int randNum() {
+ Random r = new Random();
+ // Generate random integers in range 0 to 999
+ int num = r.nextInt(1000);
+ // int num = r.nextDouble(1000);
+ return num;
+ }
+ }
 
