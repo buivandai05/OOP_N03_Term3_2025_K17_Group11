f5e8d2fca9213d9019b646f1c8fc8695e9aab7d9
Vu Huy Do <148351551+dovh11@users.noreply.github.com>
2025-05-09 08:40:35 +0700

    Update BenhNhan.java

--- src/BenhNhan.java
+++ src/BenhNhan.java
@@ -1,1 +1,74 @@
+public class BenhNhan {
+    private String hoTen;
+    private int tuoi;
+    private String gioiTinh;
+    private String diaChi;
+    private String soDienThoai;
+    private String benhLy;
 
+    public BenhNhan(String hoTen, int tuoi, String gioiTinh, String diaChi, String soDienThoai, String benhLy) {
+        this.hoTen = hoTen;
+        this.tuoi = tuoi;
+        this.gioiTinh = gioiTinh;
+        this.diaChi = diaChi;
+        this.soDienThoai = soDienThoai;
+        this.benhLy = benhLy;
+    }
+
+    public String getHoTen() {
+        return hoTen;
+    }
+
+    public void setHoTen(String hoTen) {
+        this.hoTen = hoTen;
+    }
+
+    public int getTuoi() {
+        return tuoi;
+    }
+
+    public void setTuoi(int tuoi) {
+        this.tuoi = tuoi;
+    }
+
+    public String getGioiTinh() {
+        return gioiTinh;
+    }
+
+    public void setGioiTinh(String gioiTinh) {
+        this.gioiTinh = gioiTinh;
+    }
+
+    public String getDiaChi() {
+        return diaChi;
+    }
+
+    public void setDiaChi(String diaChi) {
+        this.diaChi = diaChi;
+    }
+
+    public String getSoDienThoai() {
+        return soDienThoai;
+    }
+
+    public void setSoDienThoai(String soDienThoai) {
+        this.soDienThoai = soDienThoai;
+    }
+
+    public String getBenhLy() {
+        return benhLy;
+    }
+
+    public void setBenhLy(String benhLy) {
+        this.benhLy = benhLy;
+    }
+
+    public void hienThiThongTin() {
+        System.out.println("Họ tên: " + hoTen);
+        System.out.println("Tuổi: " + tuoi);
+        System.out.println("Giới tính: " + gioiTinh);
+        System.out.println("Địa chỉ: " + diaChi);
+        System.out.println("Số điện thoại: " + soDienThoai);
+        System.out.println("Bệnh lý: " + benhLy);
+    }
+}
