0b32e6a35032f93ca7b2b7a9b1069991d9568020
camlee <150875672+camlee05@users.noreply.github.com>
2025-05-14 23:37:03 +0700

    Create HoSoBenhAn.java

---
+++ src/HoSoBenhAn.java
@@ -0,0 +1,20 @@
+public class HoSoBenhAn {
+    private String maHoSo;
+    private String ngayKham;
+    private String trieuChung;
+    private String chuanDoan;
+    private DonThuoc donThuoc;
+    private BacSi bacSiPhuTrach;
+
+    public HoSoBenhAn(String maHoSo, String ngayKham, String trieuChung, String chuanDoan,
+                      DonThuoc donThuoc, BacSi bacSiPhuTrach) {
+        this.maHoSo = maHoSo;
+        this.ngayKham = ngayKham;
+        this.trieuChung = trieuChung;
+        this.chuanDoan = chuanDoan;
+        this.donThuoc = donThuoc;
+        this.bacSiPhuTrach = bacSiPhuTrach;
+    }
+
+    // Getter & Setter...
+}
