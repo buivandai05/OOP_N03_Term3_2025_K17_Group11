58b7c925012a7e3a896e2c55bb95ec29cc7cf626
camlee <150875672+camlee05@users.noreply.github.com>
2025-05-14 23:31:42 +0700

    Update BacSi.java

--- src/BacSi.java
+++ src/BacSi.java
@@ -1,56 +1,15 @@
-public class BacSi {
-    private String hoTen;
+import java.util.List;
+
+public class BacSi extends Nguoi {
     private String chuyenKhoa;
-    private String soDienThoai;
-    private int namKinhNghiem;
+    private List<BenhNhan> benhNhanPhuTrach;
 
-    // Constructor
-    public BacSi(String hoTen, String chuyenKhoa, String soDienThoai, int namKinhNghiem) {
-        this.hoTen = hoTen;
+    public BacSi(String maNguoi, String hoTen, String ngaySinh, String gioiTinh, String soDienThoai,
+                 String chuyenKhoa, List<BenhNhan> benhNhanPhuTrach) {
+        super(maNguoi, hoTen, ngaySinh, gioiTinh, soDienThoai);
         this.chuyenKhoa = chuyenKhoa;
-        this.soDienThoai = soDienThoai;
-        this.namKinhNghiem = namKinhNghiem;
+        this.benhNhanPhuTrach = benhNhanPhuTrach;
     }
 
-    // Getter và Setter
-    public String getHoTen() {
-        return hoTen;
-    }
-
-    public void setHoTen(String hoTen) {
-        this.hoTen = hoTen;
-    }
-
-    public String getChuyenKhoa() {
-        return chuyenKhoa;
-    }
-
-    public void setChuyenKhoa(String chuyenKhoa) {
-        this.chuyenKhoa = chuyenKhoa;
-    }
-
-    public String getSoDienThoai() {
-        return soDienThoai;
-    }
-
-    public void setSoDienThoai(String soDienThoai) {
-        this.soDienThoai = soDienThoai;
-    }
-
-    public int getNamKinhNghiem() {
-        return namKinhNghiem;
-    }
-
-    public void setNamKinhNghiem(int namKinhNghiem) {
-        this.namKinhNghiem = namKinhNghiem;
-    }
-
-    // In thông tin bác sĩ
-    public void hienThiThongTin() {
-        System.out.println("Họ tên: " + hoTen);
-        System.out.println("Chuyên khoa: " + chuyenKhoa);
-        System.out.println("SĐT: " + soDienThoai);
-        System.out.println("Năm kinh nghiệm: " + namKinhNghiem);
-    }
+    // Getter & Setter...
 }
-
