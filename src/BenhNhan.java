7f027b4917c3a8e8a7e7ddab386f1a67c073699c
camlee <150875672+camlee05@users.noreply.github.com>
2025-05-14 23:34:52 +0700

    Update BenhNhan.java

--- src/BenhNhan.java
+++ src/BenhNhan.java
@@ -1,56 +1,16 @@
-public class BenhNhan {
-    private String maBenhNhan;
-    private String tenBenhNhan;
-    private String ngaySinh;
-    private String gioiTinh;
-    private String sdtBenhNhan;
+import java.util.List;
+
+public class BenhNhan extends Nguoi {
+    private String maTheBH;
     private String diaChi;
+    private List<HoSoBenhAn> hoSoBenhAn;
 
-    public String getMaBenhNhan() {
-        return maBenhNhan;
-    }
-
-    public void setMaBenhNhan(String maBenhNhan) {
-        this.maBenhNhan = maBenhNhan;
-    }
-
-    public String getTenBenhNhan() {
-        return tenBenhNhan;
-    }
-
-    public void setTenBenhNhan(String tenBenhNhan) {
-        this.tenBenhNhan = tenBenhNhan;
-    }
-
-    public String getNgaySinh() {
-        return ngaySinh;
-    }
-
-    public void setNgaySinh(String ngaySinh) {
-        this.ngaySinh = ngaySinh;
-    }
-
-    public String getGioiTinh() {
-        return gioiTinh;
-    }
-
-    public void setGioiTinh(String gioiTinh) {
-        this.gioiTinh = gioiTinh;
-    }
-
-    public String getSdtBenhNhan() {
-        return sdtBenhNhan;
-    }
-
-    public void setSdtBenhNhan(String sdtBenhNhan) {
-        this.sdtBenhNhan = sdtBenhNhan;
-    }
-
-    public String getDiaChi() {
-        return diaChi;
-    }
-
-    public void setDiaChi(String diaChi) {
+    public BenhNhan(String maNguoi, String hoTen, String ngaySinh, String gioiTinh, String soDienThoai,
+                    String maTheBH, String diaChi, List<HoSoBenhAn> hoSoBenhAn) {
+        super(maNguoi, hoTen, ngaySinh, gioiTinh, soDienThoai);
+        this.maTheBH = maTheBH;
         this.diaChi = diaChi;
+        this.hoSoBenhAn = hoSoBenhAn;
     }
+
 }
