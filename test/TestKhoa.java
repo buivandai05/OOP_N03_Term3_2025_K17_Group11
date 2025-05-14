418f395647be2ddd9205bbebe3f6dd03d9dfc6d8
camlee <150875672+camlee05@users.noreply.github.com>
2025-05-14 23:47:13 +0700

    Delete test/TestKhoa.java

--- test/TestKhoa.java
+++
@@ -1,7 +0,0 @@
-public class TestKhoa {
-    public static void main(String[] args) {
-        Khoa khoa1 = new Khoa("W001", "Khoa Nội", 40);
-        Khoa.displayInfo(); // In thông tin khoa
-    }
-}
-
