de21a9957c2bf60e3c92855abc8ae3e9f71700ce
camlee <150875672+camlee05@users.noreply.github.com>
2025-05-14 23:39:26 +0700

    Create LichHen.java

---
+++ src/LichHen.java
@@ -0,0 +1,15 @@
+public class LichHen {
+    private String ngayHen;
+    private String gioHen;
+    private BenhNhan benhNhan;
+    private BacSi bacSi;
+
+    public LichHen(String ngayHen, String gioHen, BenhNhan benhNhan, BacSi bacSi) {
+        this.ngayHen = ngayHen;
+        this.gioHen = gioHen;
+        this.benhNhan = benhNhan;
+        this.bacSi = bacSi;
+    }
+
+    // Getter & Setter...
+}
