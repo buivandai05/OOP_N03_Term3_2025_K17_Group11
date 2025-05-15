4bc6665a6192c2b890432286221009af0ebf04a3
Vu Huy Do <148351551+dovh11@users.noreply.github.com>
2025-05-15 08:32:11 +0700

    Update HoaDonTest.java

--- Test/HoaDonTest.java
+++ Test/HoaDonTest.java
@@ -20,3 +20,28 @@ public class TestHoaDon {
         }
 
         
+// Create a BenhNhan instance
+        BenhNhan benhNhan = new BenhNhan("BN001", "Nguyen Van A");
+
+        // Create a HoaDon instance
+        HoaDon hoaDon = new HoaDon("HD001", "2025-05-15", benhNhan, 1500000.0);
+
+        // Test getters
+        System.out.println("Ma Hoa Don: " + hoaDon.getMaHoaDon());
+        System.out.println("Ngay Lap: " + hoaDon.getNgayLap());
+        System.out.println("Benh Nhan: " + hoaDon.getBenhNhan());
+        System.out.println("Tong Tien: " + hoaDon.getTongTien());
+
+        // Test setters
+        hoaDon.setMaHoaDon("HD002");
+        hoaDon.setNgayLap("2025-05-16");
+        hoaDon.setBenhNhan(new BenhNhan("BN002", "Tran Thi B"));
+        hoaDon.setTongTien(2000000.0);
+
+        // Verify updated values
+        System.out.println("Updated Ma Hoa Don: " + hoaDon.getMaHoaDon());
+        System.out.println("Updated Ngay Lap: " + hoaDon.getNgayLap());
+        System.out.println("Updated Benh Nhan: " + hoaDon.getBenhNhan());
+        System.out.println("Updated Tong Tien: " + hoaDon.getTongTien());
+    }
+}
