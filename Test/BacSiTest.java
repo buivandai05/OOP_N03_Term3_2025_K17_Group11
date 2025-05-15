565bd4d5fd69d8868f0675ec29bb497be1c78f8c
buivandai05 <151592182+buivandai05@users.noreply.github.com>
2025-05-15 08:35:29 +0700

    Update BacSiTest.java

--- Test/BacSiTest.java
+++ Test/BacSiTest.java
@@ -1,1 +1,36 @@
+public class Basi{
+  private String maBacsi;
+  private String hoten;
+  private String chuyenkhoa;
+  private String std;
+  private String lichlamviec;
 
+  public Bacsi(String maBacsi, String hoten, String chuyenkhoa, String std. String lichlamviec){
+    this.maBacsi=maBacsi;
+    this.hoTen = hoTen;
+    this.chuyenKhoa = chuyenKhoa;
+    this.soDienThoai = soDienThoai;
+    this.lichLamViec = lichLamViec;
+    }
+  public String getMaBacSi() { return maBacSi; }
+    public void setMaBacSi(String maBacSi) {
+      
+      this.maBacSi = maBacSi;
+    }
+
+    public String getHoTen() { 
+      return hoTen;
+      
+                             }
+    public void setHoTen(String hoTen) { this.hoTen = hoTen; }
+
+    public String getChuyenKhoa() { return chuyenKhoa; }
+    public void setChuyenKhoa(String chuyenKhoa) { this.chuyenKhoa = chuyenKhoa; }
+
+    public String getSoDienThoai() { return soDienThoai; }
+    public void setSoDienThoai(String soDienThoai) { this.soDienThoai = soDienThoai; }
+
+    public String getLichLamViec() { return lichLamViec; }
+    public void setLichLamViec(String lichLamViec) { this.lichLamViec = lichLamViec; }
+}
+  
