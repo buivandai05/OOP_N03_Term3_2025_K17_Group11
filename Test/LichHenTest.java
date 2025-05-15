2131a28d7761d719cdfd72015352cdeabf3212f1
Vu Huy Do <148351551+dovh11@users.noreply.github.com>
2025-05-15 08:33:27 +0700

    Update LichHenTest.java

--- Test/LichHenTest.java
+++ Test/LichHenTest.java
@@ -7,6 +7,12 @@ public class TestLichHen {
         // Create an instance of LichHen
         LichHen lichHen = new LichHen("2025-05-20", "10:00 AM", benhNhan, bacSi);
 
+        // Test getters
+        System.out.println("Ngay Hen: " + lichHen.getNgayHen());
+        System.out.println("Gio Hen: " + lichHen.getGioHen());
+        System.out.println("Benh Nhan: " + lichHen.getBenhNhan().getName());
+        System.out.println("Bac Si: " + lichHen.getBacSi().getName());
+
         
     }
 }
