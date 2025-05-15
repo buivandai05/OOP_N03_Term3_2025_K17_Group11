cd1f91e96a2eb09fd2f9bab3da0fab90e57d72e6
Vu Huy Do <148351551+dovh11@users.noreply.github.com>
2025-05-15 08:33:07 +0700

    Update LichHenTest.java

--- Test/LichHenTest.java
+++ Test/LichHenTest.java
@@ -7,24 +7,6 @@ public class TestLichHen {
         // Create an instance of LichHen
         LichHen lichHen = new LichHen("2025-05-20", "10:00 AM", benhNhan, bacSi);
 
-        // Test getters
-        System.out.println("Ngay Hen: " + lichHen.getNgayHen());
-        System.out.println("Gio Hen: " + lichHen.getGioHen());
-        System.out.println("Benh Nhan: " + lichHen.getBenhNhan().getName());
-        System.out.println("Bac Si: " + lichHen.getBacSi().getName());
-
-        // Test setters
-        lichHen.setNgayHen("2025-05-21");
-        lichHen.setGioHen("11:00 AM");
-        BenhNhan newBenhNhan = new BenhNhan("Le Thi C");
-        BacSi newBacSi = new BacSi("Dr. Nguyen Van D");
-        lichHen.setBenhNhan(newBenhNhan);
-        lichHen.setBacSi(newBacSi);
-
-        // Verify updates
-        System.out.println("Updated Ngay Hen: " + lichHen.getNgayHen());
-        System.out.println("Updated Gio Hen: " + lichHen.getGioHen());
-        System.out.println("Updated Benh Nhan: " + lichHen.getBenhNhan().getName());
-        System.out.println("Updated Bac Si: " + lichHen.getBacSi().getName());
+        
     }
 }
