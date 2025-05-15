3644678e860f53a08d670113921808fee6972db6
Vu Huy Do <148351551+dovh11@users.noreply.github.com>
2025-05-15 08:20:54 +0700

    Create NguoiTest.java

---
+++ Test/NguoiTest.java
@@ -0,0 +1,35 @@
+// Define a subclass for testing
+class TestNguoi extends Nguoi {
+    public TestNguoi(String maNguoi, String hoTen, String ngaySinh, String gioiTinh, String soDienThoai) {
+        super(maNguoi, hoTen, ngaySinh, gioiTinh, soDienThoai);
+    }
+}
+
+public class NguoiTest {
+    public static void main(String[] args) {
+        // Create an instance of TestNguoi
+        TestNguoi nguoi = new TestNguoi("001", "Nguyen Van A", "1990-01-01", "Male", "0123456789");
+
+        // Test getter methods
+        System.out.println("Ma Nguoi: " + nguoi.getMaNguoi());
+        System.out.println("Ho Ten: " + nguoi.getHoTen());
+        System.out.println("Ngay Sinh: " + nguoi.getNgaySinh());
+        System.out.println("Gioi Tinh: " + nguoi.getGioiTinh());
+        System.out.println("So Dien Thoai: " + nguoi.getSoDienThoai());
+
+        // Test setter methods
+        nguoi.setMaNguoi("002");
+        nguoi.setHoTen("Tran Thi B");
+        nguoi.setNgaySinh("1995-05-10");
+        nguoi.setGioiTinh("Female");
+        nguoi.setSoDienThoai("0987654321");
+
+        // Verify updated values
+        System.out.println("\nUpdated Information:");
+        System.out.println("Ma Nguoi: " + nguoi.getMaNguoi());
+        System.out.println("Ho Ten: " + nguoi.getHoTen());
+        System.out.println("Ngay Sinh: " + nguoi.getNgaySinh());
+        System.out.println("Gioi Tinh: " + nguoi.getGioiTinh());
+        System.out.println("So Dien Thoai: " + nguoi.getSoDienThoai());
+    }
+}
