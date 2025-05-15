72bb88be38ea075689d09e25de17a1aff6c4ba0b
Vu Huy Do <148351551+dovh11@users.noreply.github.com>
2025-05-15 08:35:21 +0700

    Update NguoiTest.java

--- Test/NguoiTest.java
+++ Test/NguoiTest.java
@@ -10,6 +10,13 @@ public class NguoiTest {
         // Create an instance of TestNguoi
         TestNguoi nguoi = new TestNguoi("001", "Nguyen Van A", "1990-01-01", "Male", "0123456789");
 
+        // Test getter methods
+        System.out.println("Ma Nguoi: " + nguoi.getMaNguoi());
+        System.out.println("Ho Ten: " + nguoi.getHoTen());
+        System.out.println("Ngay Sinh: " + nguoi.getNgaySinh());
+        System.out.println("Gioi Tinh: " + nguoi.getGioiTinh());
+        System.out.println("So Dien Thoai: " + nguoi.getSoDienThoai());
+
         
     }
 }
