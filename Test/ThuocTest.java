4fc0a9b4840ae45fb77f1891d9a1f0951d6df202
Vu Huy Do <148351551+dovh11@users.noreply.github.com>
2025-05-16 07:31:28 +0700

    Update ThuocTest.java

--- Test/ThuocTest.java
+++ Test/ThuocTest.java
@@ -3,10 +3,10 @@ public class ThuocTest {
         Thuoc thuoc = new Thuoc("Paracetamol", "Tablet", 20, "2 tablets every 4-6 hours");
 
         System.out.println("Testing Getters:");
-        System.out.println("Ten Thuoc: " + thuoc.getTenThuoc());  // Expected: Paracetamol
-        System.out.println("Don Vi Tinh: " + thuoc.getDonViTinh());  // Expected: Tablet
-        System.out.println("So Luong: " + thuoc.getSoLuong());  // Expected: 20
-        System.out.println("Lieu Dung: " + thuoc.getLieuDung());  // Expected: 2 tablets every 4-6 hours
+        System.out.println("Ten Thuoc: " + thuoc.getTenThuoc());  
+        System.out.println("Don Vi Tinh: " + thuoc.getDonViTinh());  
+        System.out.println("So Luong: " + thuoc.getSoLuong()); 
+        System.out.println("Lieu Dung: " + thuoc.getLieuDung());  
 
         System.out.println("\nTesting Setters:");
         thuoc.setTenThuoc("Aspirin");
@@ -14,9 +14,9 @@ public class ThuocTest {
         thuoc.setSoLuong(10);
         thuoc.setLieuDung("1 capsule every 8 hours");
 
-        System.out.println("Updated Ten Thuoc: " + thuoc.getTenThuoc());  // Expected: Aspirin
-        System.out.println("Updated Don Vi Tinh: " + thuoc.getDonViTinh());  // Expected: Capsule
-        System.out.println("Updated So Luong: " + thuoc.getSoLuong());  // Expected: 10
-        System.out.println("Updated Lieu Dung: " + thuoc.getLieuDung());  // Expected: 1 capsule every 8 hours
+        System.out.println("Updated Ten Thuoc: " + thuoc.getTenThuoc());  
+        System.out.println("Updated Don Vi Tinh: " + thuoc.getDonViTinh());  
+        System.out.println("Updated So Luong: " + thuoc.getSoLuong());  
+        System.out.println("Updated Lieu Dung: " + thuoc.getLieuDung()); 
     }
 }
