8467490c8df58480c1aedba1cc9af0879bf426b9
Cẩm Ly <150875672+camlee05@users.noreply.github.com>
2025-05-25 13:22:10 +0700

    Update BenhNhan.java

--- src/BenhNhan.java
+++ src/BenhNhan.java
@@ -1,35 +1,54 @@
-import java.util.List;
-
-public class BenhNhan extends Nguoi {
-    private String maTheBH;
+public class BenhNhan {
+    private String maBenhNhan;
+    private String tenBenhNhan;
+    private int tuoi;
+    private String gioiTinh;
     private String diaChi;
-    private List<HoSoBenhAn> hoSoBenhAn;
-    private PhongDieuTri phongDieuTri;
+    private String soDienThoai;
+    private String maPhong;
+    private String chuanDoan;
 
-
-    public BenhNhan(String maNguoi, String hoTen, String ngaySinh, String gioiTinh, String soDienThoai,
-                    String maTheBH, String diaChi, List<HoSoBenhAn> hoSoBenhAn,PhongDieuTri phongDieuTri) {
-        super(maNguoi, hoTen, ngaySinh, gioiTinh, soDienThoai);
-        this.maTheBH = maTheBH;
+    // Constructor
+    public BenhNhan(String maBenhNhan, String tenBenhNhan, int tuoi, String gioiTinh,
+                    String diaChi, String soDienThoai, String maPhong, String chuanDoan) {
+        this.maBenhNhan = maBenhNhan;
+        this.tenBenhNhan = tenBenhNhan;
+        this.tuoi = tuoi;
+        this.gioiTinh = gioiTinh;
         this.diaChi = diaChi;
-        this.hoSoBenhAn = hoSoBenhAn;
-        this.phongDieuTri = phongDieuTri;
+        this.soDienThoai = soDienThoai;
+        this.maPhong = maPhong;
+        this.chuanDoan = chuanDoan;
     }
 
-    public BenhNhan() {
-        super();
+    // Getters and Setters
+    public String getMaBenhNhan() { return maBenhNhan; }
+    public void setMaBenhNhan(String maBenhNhan) { this.maBenhNhan = maBenhNhan; }
+
+    public String getTenBenhNhan() { return tenBenhNhan; }
+    public void setTenBenhNhan(String tenBenhNhan) { this.tenBenhNhan = tenBenhNhan; }
+
+    public int getTuoi() { return tuoi; }
+    public void setTuoi(int tuoi) { this.tuoi = tuoi; }
+
+    public String getGioiTinh() { return gioiTinh; }
+    public void setGioiTinh(String gioiTinh) { this.gioiTinh = gioiTinh; }
+
+    public String getDiaChi() { return diaChi; }
+    public void setDiaChi(String diaChi) { this.diaChi = diaChi; }
+
+    public String getSoDienThoai() { return soDienThoai; }
+    public void setSoDienThoai(String soDienThoai) { this.soDienThoai = soDienThoai; }
+
+    public String getMaPhong() { return maPhong; }
+    public void setMaPhong(String maPhong) { this.maPhong = maPhong; }
+
+    public String getChuanDoan() { return chuanDoan; }
+    public void setChuanDoan(String chuanDoan) { this.chuanDoan = chuanDoan; }
+
+    public void hienThiThongTin() {
+        System.out.println("Mã BN: " + maBenhNhan + ", Tên: " + tenBenhNhan + ", Tuổi: " + tuoi +
+                ", Giới tính: " + gioiTinh + ", Địa chỉ: " + diaChi +
+                ", SĐT: " + soDienThoai + ", Phòng: " + maPhong + ", Chuẩn đoán: " + chuanDoan);
     }
-
-    public String getMaTheBH() {return maTheBH;}
-    public void setMaTheBH(String maTheBH) {this.maTheBH = maTheBH;}
-
-    public String getDiaChi() {return diaChi;}
-    public void setDiaChi(String diaChi) {this.diaChi = diaChi;}
-
-    public List<HoSoBenhAn> getHoSoBenhAn() {return hoSoBenhAn;}
-    public void setHoSoBenhAn(List<HoSoBenhAn> hoSoBenhAn) {this.hoSoBenhAn = hoSoBenhAn;}
-
-    public PhongDieuTri getPhongDieuTri() {return phongDieuTri;}
-    public void setPhongDieuTri(PhongDieuTri phongDieuTri) {this.phongDieuTri = phongDieuTri;}
-
 }
