b76604f4d3c6578e5cbc1e8136714816c690433c
Vu Huy Do <148351551+dovh11@users.noreply.github.com>
2025-05-29 08:49:56 +0700

    Update PhongDieuTriTest.java

--- test/PhongDieuTriTest.java
+++ test/PhongDieuTriTest.java
@@ -8,5 +8,8 @@ import static org.junit.jupiter.api.Assertions.*;
 public class PhongDieuTriTest {
     private PhongDieuTri phongDieuTri;
 
-
+    @BeforeEach
+    public void setUp() {
+        phongDieuTri = new PhongDieuTri("P001", "Phòng A", "Khoa Nội", 2, "BS001");
+    }
 }
