4d77e216d03f4680ba189448947456f9621dd93d
Cẩm Ly <150875672+camlee05@users.noreply.github.com>
2025-06-05 08:57:11 +0700

    Create Sequence.java

---
+++ review/Sequence.java
@@ -0,0 +1,38 @@
+package review;
+
+public class Sequence {
+    private Object[] objects; // array of Object
+    private int next = 0;
+
+    public Sequence(int size) {
+        objects = new Object[size];
+    }
+
+    public void add(Object x) {
+        if (next < objects.length) {
+            objects[next] = x;
+            next++;
+        }
+    }
+
+    // Inner class SSelector implements Selector
+    private class SSelector implements review.Selector {
+        int i = 0;
+
+        public boolean end() {
+            return i == objects.length;
+        }
+
+        public Object current() {
+            return objects[i];
+        }
+
+        public void next() {
+            if (i < objects.length) i++;
+        }
+    }
+
+    public review.Selector getSelector() {
+        return new SSelector();
+    }
+}
